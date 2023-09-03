56e68271cc5b3d37318f14403ead3ddf1ec939b0
ibtihajnaeem <ibtihajnaeem@ibtihajnaeems-MacBook-Pro.local>
2023-09-03 14:12:07 +0200

    Inner class with iterator

---
+++ src/com/practice/oop/innerClass/MainClass.java
@@ -0,0 +1,27 @@
+package com.practice.oop.innerClass;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ShoppingCart cart = new ShoppingCart();
+
+		// Adding items to the cart
+		cart.addItem("Laptop", 1, 899.99);
+		cart.addItem("Headphones", 2, 49.99);
+		cart.addItem("Mouse", 3, 19.99);
+
+		// Calculating and printing the total price
+		double total = cart.calculateTotalPrice();
+
+		System.out.println(cart.toString());
+
+		System.out.println("Total price of items in the cart: $" + total);
+		cart.removeItem("Headphones");
+
+		// Recalculating and printing the total price
+		total = cart.calculateTotalPrice();
+		System.out.println("Total price after removing an item: $" + total);
+		System.out.println(cart.toString());
+	}
+
+}
---
+++ src/com/practice/oop/innerClass/ShoppingCart.java
@@ -0,0 +1,92 @@
+package com.practice.oop.innerClass;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ShoppingCart {
+
+	private final List<CartItem> cart;
+
+	public ShoppingCart() {
+		super();
+		this.cart = new ArrayList<>();
+	}
+
+	@Override
+	public String toString() {
+		Iterator<CartItem> iterator = cart.iterator();
+		String data = "";
+		while (iterator.hasNext()) {
+			CartItem item = iterator.next();
+			data += item.toString() + "\n";
+		}
+		return data;
+	}
+
+	public void addItem(String productName, int quantity, double pricePerUnit) {
+		cart.add(new CartItem(productName, quantity, pricePerUnit));
+	}
+
+	public void removeItem(String productName) {
+		CartItem requestedCart = new CartItem(productName, 0, 0);
+		Iterator<CartItem> iterator = cart.iterator();
+		while (iterator.hasNext()) {
+			CartItem item = iterator.next();
+			if (item.equals(requestedCart)) {
+				iterator.remove();
+			}
+		}
+
+	}
+
+	public double calculateTotalPrice() {
+		double totalPrice = 0.0;
+		for (CartItem item : cart) {
+			totalPrice += item.getItemTotal();
+		}
+		return totalPrice;
+	}
+
+	class CartItem {
+		private final String productName;
+		private final int quantity;
+		private final double pricePerUnit;
+
+		public CartItem(String productName, int quantity, double pricePerUnit) {
+			super();
+			this.productName = productName;
+			this.quantity = quantity;
+			this.pricePerUnit = pricePerUnit;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			CartItem other = (CartItem) obj;
+			return productName == other.productName;
+		}
+
+		@Override
+		public String toString() {
+			return "CartItem [productName=" + productName + ", quantity=" + quantity + ", pricePerUnit=" + pricePerUnit
+					+ "]";
+		}
+
+		public String getProductName() {
+			return productName;
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public double getPricePerUnit() {
+			return pricePerUnit;
+		}
+
+		public double getItemTotal() {
+			return quantity * pricePerUnit;
+		}
+
+	}
+}
